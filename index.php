cde761854c57da7e7d8b913b9bf682936f0f21f6
SergioMerinoCortez <utp0001553@alumno.utpuebla.edu.mx>
2021-12-26 14:45:43 -0600

    primeras  carpetas en mvc

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once "controladores/plantilla.controlador.php";
+$plantilla = new ControladorPlantilla();
+$plantilla -> plantilla();
+
+
+
+
+/*
+or create a new repository on the command line
+echo "# ecommerce_apr" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Sergiodevelopersk8/ecommerce_apr.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/Sergiodevelopersk8/ecommerce_apr.git
+git branch -M main
+git push -u origin main
+
+
+
+
+or create a new repository on the command line
+echo "# ecommerce_apr" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Sergiodevelopersk8/ecommerce_apr.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin git@github.com:Sergiodevelopersk8/ecommerce_apr.git
+git branch -M main
+git push -u origin main
+
+ 
+
+
+
+*/
+
+
+
+?>
+
+ 
