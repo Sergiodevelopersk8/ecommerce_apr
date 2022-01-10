000b197d0c4c0c7280de9ed8ddbe57527fd6dab9
SergioMerinoCortez <utp0001553@alumno.utpuebla.edu.mx>
2022-01-09 19:46:46 -0600

    me quede en video 46

---
+++ frontend/vistas/modulos/cabezote.php
@@ -0,0 +1,232 @@
+<!---============================================
+TOP
+=============================================--->
+
+<div class="container-fluid barraSuperior" id="top">
+
+<div class="container">
+
+<div class="row">
+
+<!---============================================
+SOCIAL
+=============================================--->
+
+<div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+	<ul>
+		<li>
+			<a href="http://facebook.com/" target="_blank">
+				<i class="fa fa-facebook redSocial facebookBlanco" aria-hidden="true">
+
+				</i>
+			</a>
+		</li>
+		<li>
+			<a href="http://youtube.com/" target="_blank">
+				<i class="fa fa-youtube redSocial youtubeBlanco" aria-hidden="true">
+
+				</i>
+			</a>
+		</li>
+		<li>
+			<a href="http://twitter.com/" target="_blank">
+				<i class="fa fa-twitter redSocial twitterBlanco" aria-hidden="true">
+
+				</i>
+			</a>
+		</li>
+		<li>
+			<a href="http://instagram.com/" target="_blank">
+				<i class="fa fa-instagram redSocial instagramBlanco" aria-hidden="true">
+
+				</i>
+			</a>
+		</li>
+	</ul>
+</div>	 
+<!---============================================
+REGISTRO
+=============================================--->
+
+<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro ">
+
+<ul>
+	<li>
+		<a href="#modalIngreso" data-toggle="modal">Ingresar</a>
+	</li>
+
+	<li><a href="#modalRegistro" data-toggle="modal" >Crear cuenta</a></li>
+
+</ul>
+
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+
+<!---============================================
+HEADER
+=============================================--->
+
+<header class="container-fluid">
+<div class="container">
+
+<div class="row" id ="cabezote">
+
+<!---============================================
+logotipo
+=============================================--->
+
+<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
+	<a href="#">
+		<img src="http://localhost/ecommerce_apr/backend/vistas/img/plantilla/logo.png" class="img-responsive">
+	</a>
+</div>
+
+
+<!---============================================
+Bloque categorias y Buscar	
+=============================================--->
+<div class="col-gl-6 col-md-6 col-sm-8 col-xs-12">
+
+<!---============================================
+BOTON CATEGORIAS	
+=============================================--->	
+<div class="col-gl-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+<p>CATEGORIAS
+<span class="pull-right"><i class="fa fa-bars" aria-hidden="true"></i></span>
+</p>
+
+
+</div>
+
+<!---============================================
+BUSCADOR	
+=============================================--->
+<div class="input col-gl-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+<input type="search" name="buscar" class="form-control" placeholder="Buscar....">
+<span class="input-group-btn">
+
+<a href="#">
+	<button class="btn btn-default backColor" type="submit">
+		<i class="fa fa-search"> </i>
+	</button>
+</a>
+
+
+</span>
+</div>
+
+</div>
+<!---============================================
+Carrito	de compras
+=============================================--->
+<div class="col-gl-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+
+<a href="#">
+
+<button class="btn btn-default pull-left backColor">
+	<i class="fa fa-shopping-cart" aria-hidden="true">  </i>
+</button>
+
+</a>
+<p>Tu Cesta<span class="cantidadCesta"></span> <br> USD $ <span class="sumaCesta"></span></p>
+</div>
+
+</div>
+
+<!---============================================
+Categorias
+=============================================--->
+<div class="col-xs-12 backColor" id="categorias">
+
+<div class="col-gl-2 col-md-2 col-sm-4 col-xs-12">
+
+<h4>
+	<a href="#" class="pixelCategorias">Lorem ipsum dolor sit</a>
+</h4>
+<hr>
+<ul>
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+</ul>
+</div>
+
+<div class="col-gl-2 col-md-2 col-sm-4 col-xs-12">
+
+<h4>
+	<a href="#" class="pixelCategorias">Lorem ipsum dolor sit</a>
+</h4>
+<hr>
+<ul>
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+</ul>
+</div>
+
+<div class="col-gl-2 col-md-2 col-sm-4 col-xs-12">
+
+<h4>
+	<a href="#" class="pixelCategorias">Lorem ipsum dolor sit</a>
+</h4>
+<hr>
+<ul>
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+</ul>
+</div>
+
+<div class="col-gl-2 col-md-2 col-sm-4 col-xs-12">
+
+<h4>
+	<a href="#" class="pixelCategorias">Lorem ipsum dolor sit</a>
+</h4>
+<hr>
+<ul>
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+	
+	<li><a href="#" class="pixelCategorias">Lorem</a></li>
+</ul>
+</div>
+
+
+</div>
+
+</div>	
+
+</header>
+
+
--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -3,10 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Box gift</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+   <meta name="title" content="tienda virtual">
+   <meta name="description" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolorem odio sed quam tenetur dolore quos, corporis id aliquid suscipit maiores praesentium quaerat. Voluptate sint iure harum nostrum, excepturi doloribus?">
+   <meta name="keyword" content="Lorem ipsum dolor, sit amet consectetur, adipisicing elit. Animi dolorem odio, sed quam tenetur dolore quos, corporis id aliquid suscipit maiores praesentium quaerat. Voluptate sint iure harum nostrum, excepturi doloribus?">
+   <title>Box gift</title>
+  
+   <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+   <link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+   <script src="vistas/js/plugins/jquery.min.js"></script>
+   <script src="vistas/js/plugins/bootstrap.min.js"></script>
 </head>
 <body>
-    <h1>Box gift</h1>
+
+<?php
+    /*=======================================
+        CABEZOTE
+    ========================================*/
+    include "modulos/cabezote.php";
+
+?>
 </body>
 </html>
