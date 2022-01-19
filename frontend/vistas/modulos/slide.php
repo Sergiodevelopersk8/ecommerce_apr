a7173b79d11120c06c02d91e445418162b6fdd1c
SergioMerinoCortez <utp0001553@alumno.utpuebla.edu.mx>
2022-01-19 16:10:32 -0600

    slide de imagen dos video 59

--- frontend/vistas/modulos/slide.php
+++ frontend/vistas/modulos/slide.php
@@ -45,7 +45,41 @@ SLIDESHOW
 </div>
 
 </li>
-		</ul>
+
+		<!--=====================================
+		SLIDE2
+		======================================-->
+
+		<li>
+	<img src="http://localhost/ecommerce_apr/backend/vistas/img/slide/default/back_default.jpg" alt="">
+
+<div class="slideOpciones slideOpcion2">
+
+<img class="imgProducto" src="http://localhost/ecommerce_apr/backend/vistas/img/slide/slide2/curso.png">
+
+<div class="textosSlide">
+	<h1>Lorem ipsum </h1>
+	<h2>Lorem ipsum dolor site </h2>
+	
+	<h3>Lorem ipsum dolor site </h3>
+
+<a href="#">
+	<button class="btn btn-default backColor text-uppercase">
+		VER PRODUCTO <span class="fa fa-chevron-right"></span>
+	
+	</button>
+</a>
+
+
+</div>
+
+</div>
+
+</li>
+
+
+
+</ul>
 
 	
 
